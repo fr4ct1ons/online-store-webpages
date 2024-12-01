aab436dd21d096e1aecc4924ddc1f8780a918cae
Marco <marco.fernandes@rarolabs.com.br>
2024-12-01 20:01:10 -0300

    feat: add calls to create, login and delete user

--- src/index.js
+++ src/index.js
@@ -23,9 +23,94 @@ const router = createBrowserRouter([
   },
   {
     path: "/storefront",
-    element: <Storefront/>
+    element: <Storefront />
   }
 ])
+
+
+class UserManager {
+  async createUser(name, password) {
+    return fetch('http://localhost:5029/User/CreateUser', {
+      method: "POST",
+      body: JSON.stringify({
+        'name': name,
+        'password': password,
+      }),
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json'
+      }
+
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('There was a problem creating an user:', error);
+      });
+  }
+  async runEverything() {
+    await this.createUser('marco', '1234')
+    var id = await this.login('marco', '1234');
+    console.log(id);
+    await this.deleteUser(id['id']);
+  }
+  async deleteUser(userId) {
+    return fetch('http://localhost:5029/User/DeleteUser?' +  'userId='+userId, {
+      method: "DELETE",
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json'
+      }
+
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(response => {
+        return response;
+      })
+      .catch(error => {
+        console.error('There was a problem creating an user:', error);
+      });
+  }
+
+
+  async login(name, password) {
+    return fetch('http://localhost:5029/User/LoginUser', {
+      method: "POST",
+      body: JSON.stringify({
+        'username': name,
+        'password': password,
+      }),
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json'
+      }
+
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      }).then(response => {
+        return response;
+      })
+      .catch(error => {
+        console.error('There was a problem logging in:', error);
+      });
+
+
+  }
+}
+var mng = new UserManager().runEverything();
 
 root.render(
   <React.StrictMode>
