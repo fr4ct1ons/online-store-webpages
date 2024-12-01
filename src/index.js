5a344aa0d3c1379a05642add60aa86333d6cbcf6
Marco <marco.fernandes@rarolabs.com.br>
2024-12-01 20:38:25 -0300

    feat: export user_maanger

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { theme } from './theme';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import NotFound from './pages/NotFound';
 import Storefront from './pages/Storefront';
-
+import { UserManager } from './login/user_manager.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router = createBrowserRouter([
@@ -28,88 +28,7 @@ const router = createBrowserRouter([
 ])
 
 
-class UserManager {
-  async createUser(name, password) {
-    return fetch('http://localhost:5029/User/CreateUser', {
-      method: "POST",
-      body: JSON.stringify({
-        'name': name,
-        'password': password,
-      }),
-      headers: {
-        'Accept': '*/*',
-        'Content-Type': 'application/json'
-      }
 
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error('There was a problem creating an user:', error);
-      });
-  }
-  async runEverything() {
-    await this.createUser('marco', '1234')
-    var id = await this.login('marco', '1234');
-    console.log(id);
-    await this.deleteUser(id['id']);
-  }
-  async deleteUser(userId) {
-    return fetch('http://localhost:5029/User/DeleteUser?' +  'userId='+userId, {
-      method: "DELETE",
-      headers: {
-        'Accept': '*/*',
-        'Content-Type': 'application/json'
-      }
-
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(response => {
-        return response;
-      })
-      .catch(error => {
-        console.error('There was a problem creating an user:', error);
-      });
-  }
-
-
-  async login(name, password) {
-    return fetch('http://localhost:5029/User/LoginUser', {
-      method: "POST",
-      body: JSON.stringify({
-        'username': name,
-        'password': password,
-      }),
-      headers: {
-        'Accept': '*/*',
-        'Content-Type': 'application/json'
-      }
-
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      }).then(response => {
-        return response;
-      })
-      .catch(error => {
-        console.error('There was a problem logging in:', error);
-      });
-
-
-  }
-}
 var mng = new UserManager().runEverything();
 
 root.render(
